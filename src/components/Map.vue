98eb7e6e5c1b89ce788ee4bc9fb37bd91ded98cf
Helene Monefeldt <71066142+Zentia-DK@users.noreply.github.com>
2024-04-15 10:07:45 +0200

    map

---
+++ src/components/Map.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div class="Map">
+    <h1 style="color: black;">Map</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
