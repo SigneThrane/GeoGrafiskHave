99920c4ee7bccc6b81c32e208aac0cee0aaed6bf
Helene Monefeldt <71066142+Zentia-DK@users.noreply.github.com>
2024-04-26 09:14:47 +0200

    i18n

--- src/components/forside.vue
+++ src/components/forside.vue
@@ -22,6 +22,14 @@ const getLocation = () => {
   
 }
 
+const changeLocale = () => {
+  if (router.locale.value === 'da') {
+    router.push({ name: 'forside', params: { lang: 'da' } });
+  } else {
+    router.push({ name: 'forside', params: { lang: 'en' } });
+  }
+  console.log(router.locale.value);
+}
 
 </script>
 
@@ -32,21 +40,21 @@ const getLocation = () => {
    </div>
       <div class="forside">    
         <div class="text">
-        <p class="overskrift2"> Geografisk Have</p>
+        <p class="overskrift2"> {{ $t('Geografisk Have') }}</p>
       
-        <p class="overskrift">Plantejægerne</p>
+        <p class="overskrift">{{ $t('Plantejærgerne') }}</p>
        
         <RouterLink to="/map">
-        <button class="big-button" @click="getLocation">Forsæt</button>
+        <button class="big-button" @click="getLocation">{{ $t('Fortsæt') }}</button>
         </RouterLink>
 
         <div class="small-buttons">
        
-          <button class="small-button1" @click="confirmAction">Dansk <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-globe" viewBox="-4 -4 20 20">
+          <button class="small-button1" @click="changeLocale">{{ $t('Dansk') }}<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-globe" viewBox="-4 -4 20 20">
           <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
           </svg></button>
                            
-          <button class="small-button2" @click="confirmAction">English <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-globe" viewBox="-4 -4 20 20">
+          <button class="small-button2" @click="changeLocale">{{ $t('Engelsk') }}<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-globe" viewBox="-4 -4 20 20">
           <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
           </svg></button>
         
--- src/main.js
+++ src/main.js
@@ -6,10 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import { createI18n } from 'vue-i18n'
+
+
+const i18n = createI18n({
+    locale: 'da',
+    messages: {
+        en: {
+        message: {
+            GeografiskHave: 'Geografisk Garden',
+        }
+        },
+        da: {
+        message: {
+            GeographocalGarden: 'Geografisk Have'
+        }
+        }
+    }
+    })
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')
